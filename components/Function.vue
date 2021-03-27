a24dcfc5f0a5f508f4306698cc09fb253d4d8011
KauanRakoski <kauan.k.rakoski@gmail.com>
2021-03-27 00:46:52 +0000

    .

---
+++ components/Function.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <img width="130px" src="https://globaleducationcoalition.unesco.org/uploads/222_Microbit_Educational_Foundation_color_logo_L1JPej.png?v=drqPpPus0q06Il2aCFAaocVL7eS295yXh0xY9eWkvJ0">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Function'
+}
+</script>
+
+<style scoped>
+  .container{
+    background-color: #fff;
+    border: 1px solid;
+    width: 230px;
+    height: 230px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 240ms ease-out;
+  }
+  .container:hover{
+    cursor: pointer;
+    transform: translateY(-15px);
+    box-shadow: 3px 3px 4px rgba(0, 0, 0, 0.3);
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>Select a functionality</h3>
+  <div class="utilities-container">
+    <div v-for="i in 20" :key="i">
+      <Function/>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600&display=swap');
+
+  h3{
+    margin: 30px 0;
+    font-family: "Inter", sans-serif;
+    width: 100%;
+    text-align: center;
+    font-size: 40px;
+    font-weight: bold;
+  }
+  .utilities-container{
+    width: 100%;
+    display: grid;
+    justify-content: center;
+    grid-template-columns: repeat(5, 230px);
+  }
+</style>
